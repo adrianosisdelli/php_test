cbc15a9a659de83b9c6a0bebaecbd803a2e6d705
Adriano Sisdelli Costa <adriano.costa@ppgj.local>
2019-03-01 15:19:23 -0300

    Funcional 1

--- index.php
+++ index.php
@@ -9,17 +9,48 @@ $application_name = 'Test Api';
 	<title><?php echo($application_name); ?></title>
 </head>
 <body style="font-family: Verdana; font-size: 12px;">
-	<h3><?php echo($application_name); ?></h3>
-	<p>Dados:</p>
-	<button id="btnTeste">Teste</button>
+	<div class="container" style="display: flex;">
+		<div style="margin-right: 100px;">
+			<h3><?php echo($application_name); ?></h3>
+			<p>Dados:</p>
+			<button id="btnTeste">Teste</button>
+		</div>
+		<div>
+			<table>
+				<tr>
+					<td>Developer</td>
+					<td id="devName"></td>
+				</tr>
+				<tr>
+					<td>Message</td>
+					<td id="message"></td>
+				</tr>
+				<tr>
+					<td>Email</td>
+					<td id="document"></td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	
 	<script type="text/javascript">
 		$(document).ready(function(){
 
-			$("btnTeste").click(function(){
-				$.ajax({
-					url: 'adrsisdelli.ddns.net:3000/adriano.costa',
-					success: function(data) {
-						alert(data);
+			$("#btnTeste").click(function(){
+				$.ajax(
+				{
+					type: 'GET',
+					url: 'http://adrsisdelli.ddns.net:3000/adriano.costa',
+					dataType: 'json',
+					crossDomain: true,
+					async: true,
+					success: function (data) {
+						$("#devName").html('<strong>' + data.developer + '</strong>');
+						$("#message").html('<strong>' + data.message + '</strong>');
+						$("#document").html('<strong>' + data.values[4].email + '</strong>');
+					},
+					error: function(data) {
+						alert(JSON.stringify(data));
 					}
 				});
 			});
