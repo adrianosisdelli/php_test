17ecbf4ba9a77ecc2d73a1a6ad3f2977f29e7f18
Adriano Sisdelli Costa <adriano.costa@ppgj.local>
2019-03-01 11:06:33 -0300

    Inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+	$application_name = 'Test Api';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<script
+	src="https://code.jquery.com/jquery-3.3.1.slim.js"
+	integrity="sha256-fNXJFIlca05BIO2Y5zh1xrShK3ME+/lYZ0j+ChxX2DA="
+	crossorigin="anonymous"></script>
+	<title><?php echo($application_name); ?></title>
+</head>
+<body style="font-family: Verdana; font-size: 12px;">
+	<h3><?php echo($application_name); ?></h3>
+	<p>Dados:</p>
+</body>
+</html>
